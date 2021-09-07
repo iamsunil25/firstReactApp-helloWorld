fd13176dc56e7453d139fe782d5e988d81bc8611
= <sunil.17jccs011@jietjodhpur.ac.in.com>
2021-09-07 22:59:38 +0530

    first react app

---
+++ src/index.js
@@ -0,0 +1,83 @@
+
+import React from 'react';
+import  ReactDOM from 'react-dom';
+
+const Greeting = ()=>{
+  return <h1>hello this is firstReactApp like "Helloworld"</h1>;
+}
+ReactDOM.render(<Greeting/>, document.getElementById("root"));
+// function Greeting(){
+//   return <h1>hello this is firstReactApp like helloworld</h1>
+// }
+// ReactDOM.render(<Greeting></Greeting>,document.getElementById('root'))
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+
+// const  Greeting = () =>
+// {
+// return(
+//   <div Style = "display:flex">
+// <Books></Books>
+// <Books></Books>
+// <Books></Books>
+// <Books></Books>
+// <Books></Books>
+// <Books></Books>
+// <Books></Books>
+// <Books></Books>
+// <Books></Books>
+// <Books></Books>
+// </div>
+// )
+
+/*
+ function name must be capital and attribute must be capitalize
+ nested component
+const Nitesh = () =>  <h2>nitesh joshi</h2>;
+
+const Ashok = ()=> <h2>ashok joshi</h2>;
+
+const Jaswant = ()=> <h2>jaswant joshi</h2>;
+<Nitesh />
+<Ashok></Ashok>
+<Jaswant></Jaswant> */
+  // return (
+  //   <h1>sss</h1>
+  // )
+// return React.createElement('h1',{},React.createElement('h1',{},'hello i am inside'))
+
+// }
+// const Books = ()=>{
+//   return (
+//     <div>
+//       <img src="https://www.online-image-editor.com/online-image-editor-logo.png" width ="200px" height="200px" alt="dd"  />
+//       <Title></Title>
+//       <Author></Author>
+//     </div>
+//   )
+// }
+//  const Title = ()=>{
+//    return <p>The Road Not Taken</p>
+//  }
+//  const Author = ()=>{
+//    return <h1>Robert Frost</h1>
+//  }
+
+
